d83ae7c91a9d34b9e5d6e66cdf9b41c31cd4529e
secwow <dima.rebenko@gmail.com>
2022-02-27 18:07:28 +0100

    Update cells design

--- RussianSiteChecker/Cells/AvalibilityTableViewCell.swift
+++ RussianSiteChecker/Cells/AvalibilityTableViewCell.swift
@@ -9,21 +9,61 @@ import UIKit
 
 class AvalibilityTableViewCell: UITableViewCell {
     
+    @IBOutlet weak var backgroudView: UIView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var avalibilityView: UIView!
     @IBOutlet weak var lastUpdateCell: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        avalibilityView.backgroundColor = .green
+        backgroudView.layer.cornerRadius = 4.0
+        backgroudView.layer.shadowColor = UIColor.black.cgColor
+        backgroudView.layer.shadowRadius = 20
+//        avalibilityView.backgroundColor = .green
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        stopTimer()
     }
     
     private var url: URL?
+    private var date = Date()
     
     func setupWith(model: AvalibilityViewModel) {
         self.nameLabel.text = model.name
         self.url = model.url
-        self.avalibilityView.backgroundColor = model.avaliable ? UIColor.green : UIColor.red
+        self.avalibilityView.backgroundColor = model.avaliable ? UIColor.green : UIColor(red: 0.8, green: 0.173, blue: 0.149, alpha: 1)
+        self.lastUpdateCell.text = "Обновлено недавно"
+        date = Date()
+        startTimer()
+    }
+    
+    private var timer: Timer?
+    
+    private func startTimer() {
+        timer = .scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { [weak self] timer in
+            guard let self = self else { return }
+            let date = self.date.timeIntervalSinceNow
+            let calendar = Calendar.current
+            let components = calendar.dateComponents([.minute, .second], from: Date(timeIntervalSinceNow: date))
+             let second = components.second ?? 0
+            
+            var resultString: String = "Обновлено "
+            
+            if second > 0 {
+                resultString += "\(second) секунд назад"
+            } else {
+                resultString = "Обновлено недавно"
+            }
+            
+            self.lastUpdateCell.text = resultString
+        })
+    }
+    
+    private func stopTimer() {
+        timer?.invalidate()
+        timer = nil
     }
     
     @IBAction func copyAction(_ sender: Any) {
